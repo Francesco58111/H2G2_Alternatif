d5b7a0604a77126df07bd3980faa653bde2ed363
Nils Vandamme <nils.vandamme98@gmail.com>
2018-12-03 16:54:14 +0100

    Scène Nils + effet étoiles
    scène pour Nils créée, effets hyperespace + script

---
+++ Assets/Scripts/HyperspaceParticlesTransitions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HyperspaceParticlesTransitions : MonoBehaviour {
+
+    public ParticleSystemRenderer HyperSpaceParticlesSmall;
+    public ParticleSystemRenderer HyperSpaceParticlesLarge;
+
+    public float NormalSpeedLenghtScale;
+    public float LightSpeedLenghtScale;
+
+    // Use this for initialization
+    void Start () {
+        HyperSpaceParticlesSmall.lengthScale = NormalSpeedLenghtScale;
+        HyperSpaceParticlesLarge.lengthScale = NormalSpeedLenghtScale;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        /*if (Input.GetKey(KeyCode.A))
+        {
+            SetToLightSpeed();
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            SetToNormalSpeed();
+        }*/
+    }
+
+    void SetToLightSpeed()
+    {
+        HyperSpaceParticlesSmall.lengthScale = Mathf.Lerp(HyperSpaceParticlesSmall.lengthScale, LightSpeedLenghtScale, 2*Time.deltaTime);
+        HyperSpaceParticlesLarge.lengthScale = Mathf.Lerp(HyperSpaceParticlesLarge.lengthScale, LightSpeedLenghtScale, 2*Time.deltaTime);
+    }
+
+    void SetToNormalSpeed()
+    {
+        HyperSpaceParticlesSmall.lengthScale = Mathf.Lerp(HyperSpaceParticlesSmall.lengthScale, NormalSpeedLenghtScale, 2*Time.deltaTime);
+        HyperSpaceParticlesLarge.lengthScale = Mathf.Lerp(HyperSpaceParticlesLarge.lengthScale, NormalSpeedLenghtScale, 2*Time.deltaTime);
+    }
+
+}
