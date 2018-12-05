339cf38b54570593ff13427548912bee77dc0dff
zageen <zageen60@gmail.com>
2018-12-05 19:18:26 +0100

    Ennemies Shooter
    fix ennemies bullet

---
+++ Assets/Scripts/Ennemy/EnnemieShooterBehavior.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class EnnemieShooterBehavior : MonoBehaviour {
+
+    [Header("Déplacement Ennemy")]
+    public Rigidbody ennemyRigidbody;
+
+    public float ennemySpeed = 100;
+    private float delay;
+    public float moveTime = 4;
+    public float reloadDelay = 5;
+
+
+    [Header("Vie de l'ennemy")]
+    [SerializeField]
+    int ennemyHealth = 3;
+
+    public GameObject ennemieBullet;
+
+    public GameObject parentObject;
+
+
+
+
+    private void Start()
+    {
+        //Tir toutes les "firetime" seconde(s)
+        ennemyHealth = 1;
+        moveTime = 1;
+
+    }
+    private void Awake()
+    {
+        StartCoroutine(Shoot());
+    }
+    void Update()
+    {
+        CheckingHealth();
+        if ( moveTime >0)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * ennemySpeed);
+            moveTime -= Time.deltaTime;
+        }
+        
+
+
+
+
+    }
+
+    /// <summary>
+    /// Inflige des dégâts
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakingDamage(int damage)
+    {
+        ennemyHealth -= damage;
+    }
+
+
+    /// <summary>
+    /// Check la vie de l'ennemy
+    /// </summary>
+    private void CheckingHealth()
+    {
+        if (ennemyHealth < 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Collision avec le player _ GAME OVER
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            DeadCollision();
+        }
+    }
+
+
+    /// <summary>
+    /// Désactivation de l'ennemy pour le pooling
+    /// </summary>
+    private void DeadCollision()
+    {
+        //Destroy(this.gameObject);
+    }
+
+    IEnumerator Shoot()
+    {
+        for (; ;)
+        {
+            Instantiate<GameObject>(ennemieBullet,gameObject.transform.position,Quaternion.identity);
+            print("is in coroutine");
+            yield return new WaitForSeconds(reloadDelay);
+        }
+
+    }
+
+}
+
---
+++ Assets/Scripts/Ennemy/EnnemieShooterSpawner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemieShooterSpawner : MonoBehaviour {
+
+    public bool isGameRunning = false;
+
+    public GameObject ennemieShooter;
+
+    //spawnrate
+    private float delay ;
+    public float spawnDelay = 8;
+
+    //spawnrange
+    public float minSpawnRangeX = 20;
+    public float minSpawnRangeY = 20;
+    private float maxSpawnRangeX = 20;
+    private float maxSpawnRangeY = 20;
+
+    public Transform spawnerObjectPosition;
+
+    private Vector3 spawnPosition;
+
+    private void Start()
+    {
+        isGameRunning = true;
+        maxSpawnRangeX = -minSpawnRangeX;
+        maxSpawnRangeY = -minSpawnRangeY;
+        StartCoroutine(TestSpawn());
+    }
+
+	
+
+	void Update ()
+    {
+        spawnPosition = new Vector3(Random.Range(minSpawnRangeX,maxSpawnRangeX),Random.Range(minSpawnRangeY,maxSpawnRangeY),spawnerObjectPosition.position.z);
+	}
+
+    void StartMovement()
+    {
+
+    }
+
+    IEnumerator TestSpawn()
+    {
+        while (isGameRunning)
+        {
+            delay += Time.deltaTime;
+
+            if (delay > spawnDelay)
+            {
+                delay = 0;
+
+                Spawn();
+            }
+
+            yield return null;
+        }
+    }
+
+    void Spawn()
+    {
+        GameObject Temp = Instantiate<GameObject>(ennemieShooter, spawnPosition, Quaternion.Euler(90,0,0));
+        Temp.transform.SetParent(this.transform);
+    }
+}
